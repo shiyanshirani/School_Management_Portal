dde1ebdad4a50a9c8db78a0b036e0e22b1f0df96
shiyanshirani <shiyan99s@gmail.com>
2021-06-21 17:01:25 +0530

    generic(create/list_view added for CRUD

--- tremendo_api/api_app/api.py
+++ tremendo_api/api_app/api.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from .serializer import StudentSerializer, TeacherSerializer, BatchSerializer
+from .serializers import StudentSerializer, TeacherSerializer, BatchSerializer
 from .models import Student, Teacher, Batch
 
+# CREATE API VIEW
 class StudentCreateApi(generics.CreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
@@ -12,5 +13,18 @@ class TeacherCreateApi(generics.CreateAPIView):
     serializer_class = TeacherSerializer
 
 class BatchCreateApi(generics.CreateAPIView):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
+
+# LIST API VIEW
+class StudentApi(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherApi(generics.ListAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class BatchApi(generics.ListAPIView):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
--- tremendo_api/api_app/urls.py
+++ tremendo_api/api_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .api import StudentCreateApi
+from .api import StudentCreateApi, TeacherCreateApi, BatchCreateApi, StudentApi, TeacherApi, BatchApi
 
 
 urlpatterns = [
-    path('api/create', StudentCreateApi.as_view()),
+    path('student/create', StudentCreateApi.as_view()),
+    path('teacher/create', TeacherCreateApi.as_view()),
+    path('batch/create', BatchCreateApi.as_view()),
+
+    path('student/', StudentApi.as_view()),
+    path('teacher/', TeacherApi.as_view()),
+    path('batch/', BatchApi.as_view()),
 ]
