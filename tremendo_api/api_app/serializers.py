724fef28b4e5fa83269b9650126b2e9886862722
shiyanshirani <shiyan99s@gmail.com>
2021-06-23 08:18:24 +0530

    BatchSerializer fixed

--- tremendo_api/api_app/serializers.py
+++ tremendo_api/api_app/serializers.py
@@ -13,5 +13,5 @@ class TeacherSerializer(serializers.ModelSerializer):
 
 class BatchSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Student 
+        model = Batch 
         fields = '__all__'
