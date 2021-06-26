faec81578902eebf5c6d64fae0ef292d29bffa56
shiyanshirani <shiyan99s@gmail.com>
2021-06-26 13:54:37 +0530

    new urls for class based views

--- tremendo_api/api_app/api/urls.py
+++ tremendo_api/api_app/api/urls.py
@@ -3,14 +3,14 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('students/', views.student, name='student-details'),
-    path('students/<int:pk>/', views.student_api, name='student-changes'),
+    path('students/', views.StudentList.as_view(), name='student-details'),
+    path('students/<int:pk>', views.StudentDetail.as_view(), name='student-details'),
 
-    path('teachers/', views.teacher, name='teacher-details'),
-    path('teachers/<int:pk>/', views.teacher_api, name="teacher-changes"),
+    path('teachers/', views.TeacherList.as_view(), name='teacher-details'),
+    path('teachers/<int:pk>', views.TeacherDetail.as_view(), name="teacher-changes"),
 
-    path('batch/', views.batch, name="batch-details"),
+    path('batch/', views.BatchList.as_view(), name="batch-details"),
 
     path('api/token/', TokenObtainPairView.as_view(), name="get-token"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh-token")
+    path('api/token/refresh', TokenRefreshView.as_view(), name="refresh-token")
 ]
