7abfbc472026b89f91c76978cf333511c221d5c5
shiyanshirani <shiyan99s@gmail.com>
2021-06-28 13:12:40 +0530

    api-auth authentication added for better security

--- tremendo_api/tremendo_api/urls.py
+++ tremendo_api/tremendo_api/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_app.api.urls'))
+    path('', include('api_app.api.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
 ]
