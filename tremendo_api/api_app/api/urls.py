c9cf9c3bec03a2a531d8ccb6655485b65cc4cf66
shiyanshirani <shiyan99s@gmail.com>
2021-06-29 14:35:08 +0530

    pagination added to all list views

--- tremendo_api/api_app/api/urls.py
+++ tremendo_api/api_app/api/urls.py
@@ -4,12 +4,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('students/', views.StudentList.as_view(), name='student-details'),
+    path('students/list/', views.StudentListView.as_view(), name='studentlist_view'),
     path('students/<int:pk>', views.StudentDetail.as_view(), name='student-details'),
 
     path('teachers/', views.TeacherList.as_view(), name='teacher-details'),
+    path('teachers/list/', views.TeacherListView.as_view(), name='teacherlist_view'),
     path('teachers/<int:pk>', views.TeacherDetail.as_view(), name="teacher-changes"),
 
     path('batch/', views.BatchList.as_view(), name="batch-list"),
+    path('batch/list/', views.BatchListView.as_view(), name="batchlist_view"),
     path('batch/<int:pk>', views.BatchDetail.as_view(), name="batch-details"),
 
     path('api/token/', TokenObtainPairView.as_view(), name="get-token"),
