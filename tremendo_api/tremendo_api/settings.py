2f54ec5184c05d1631c4cad01151aaf3aad3fb7b
shiyanshirani <shiyan99s@gmail.com>
2021-06-29 14:37:49 +0530

    pagination_class added globally withing project

--- tremendo_api/tremendo_api/settings.py
+++ tremendo_api/tremendo_api/settings.py
@@ -128,9 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':(
-                'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
         ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        )
+        ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
