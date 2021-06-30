da77db32cddf3bd712dacafd1f36f0ef5bbe27f1
shiyanshirani <shiyan99s@gmail.com>
2021-07-01 00:48:51 +0530

    changes in apps.py to add signals

--- tremendo_api/api_app/apps.py
+++ tremendo_api/api_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_app'
+
+    def ready(self):
+        import api_app.api.signals
